5d941074a7585314669f4d5935a80a8199d3390e
Lorenzo Del Sordo <lorenzo@Lorenzos-MacBook-Pro.local>
2024-02-20 18:11:06 +0100

    add tailwind

---
+++ src/Light.tsx
@@ -0,0 +1,11 @@
+const Light = () => {
+  return (
+    <div className="flex gap-3 text-green-600">
+      <div className="bg-blue-200 p-4 rounded-xl shadow-md">test</div>
+      <div className="bg-blue-200 p-4 rounded-xl shadow-md">test</div>
+      <div className="bg-blue-200 p-4 rounded-xl shadow-md">test</div>
+    </div>
+  );
+};
+
+export default Light;
